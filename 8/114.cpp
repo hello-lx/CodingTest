be293734d598011357a63f19e9043241244458ff
yuanlixiong <476954524@qq.com>
2020-03-04 23:31:31 +0800

    h

---
+++ 8/114.cpp
@@ -0,0 +1,23 @@
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    /**
+     * @param m: positive integer (1 <= m <= 100)
+     * @param n: positive integer (1 <= n <= 100)
+     * @return: An integer
+     */
+    int uniquePaths(int m, int n) {
+        // write your code here'
+        vector<vector<int>> dp(m, vector<int>(n, 0));
+        for(int i=0; i<m; i++) dp[i][0] = 1;
+        for(int j=0; j<n; j++) dp[0][j] = 1;
+        
+        for(int i=1; i<m; i++)
+            for(int j=1; j<n; j++)
+                dp[i][j] += dp[i-1][j] + dp[i][j-1];
+                
+        return dp[m-1][n-1];
+    }
+};
