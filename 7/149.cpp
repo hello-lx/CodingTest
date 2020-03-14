889d742d468efabaee76c89a953d3101337afbc7
yuanlixiong <476954524@qq.com>
2020-03-14 13:03:49 +0800

    add

--- 7/149.cpp
+++ 7/149.cpp
@@ -1,1 +1,23 @@
+#include <algorithm>
+#include <vector>
+using namespace std;
 
+class Solution {
+public:
+    /**
+     * @param prices: Given an integer array
+     * @return: Maximum profit
+     */
+    int maxProfit(vector<int> &prices) {
+        if(prices.size() == 0) return 0;
+        
+        int minPro = prices[0];
+        int maxPro = 0;
+        for (int i=0; i<prices.size(); i++)
+        {
+            minPro = min(minPro, prices[i]);
+            maxPro = max(maxPro, prices[i] - minPro);
+        }
+        return maxPro;
+    }
+};
--- 7/150.cpp
+++ 7/150.cpp
@@ -1,1 +1,21 @@
+#include <algorithm>
+#include <vector>
+using namespace std;
 
+class Solution {
+public:
+    /**
+     * @param prices: Given an integer array
+     * @return: Maximum profit
+     */
+    int maxProfit(vector<int> &prices) {
+        // write your code here
+        if(prices.size() == 0) return 0;
+        
+        int res = 0;
+        for(int i=1; i<prices.size(); i++){
+            res += max(0, prices[i]-prices[i-1]);
+        }
+        return res;
+    }
+};
