a430534477efbdbe167abaa56f1edca9724f18db
hello-lx <476954524@qq.com>
2022-01-27 19:24:47 +0800

    test

--- 3/609.cpp
+++ 3/609.cpp
@@ -15,4 +15,37 @@ using namespace std;
 #define INT_MIN -99999999999
 #define INT_MAX 99999999999
 
-
+class Solution {
+public:
+    /**
+     * @param nums: an array of integer
+     * @param target: an integer
+     * @return: an integer
+     */
+    int twoSum5(vector<int> &nums, int target) {
+        // write your code here
+        
+        int res = 0;
+        int begin = 0;
+        int end = nums.size() - 1;
+        
+        if(nums.size() == 0) return 0;
+        
+        sort(nums.begin(), nums.end());
+        
+        while(begin < end)
+        {
+            if(nums[begin] + nums[end] <= target)
+            {
+                res += (end - begin); // 注意这里
+                ++begin;
+            }
+            else
+            {
+                --end;
+            }
+        }
+    
+        return res;
+    }
+};
--- 3/610.cpp
+++ 3/610.cpp
@@ -16,3 +16,35 @@ using namespace std;
 #define INT_MAX 99999999999
 
 
+class Solution {
+public:
+    /**
+     * @param nums: an array of Integer
+     * @param target: an integer
+     * @return: [num1, num2] (num1 < num2)
+     */
+    vector<int> twoSum7(vector<int> &nums, int target) {
+        if (nums.empty() || nums.size() < 2) {
+            return {-1, -1};
+        }
+        
+        // target 要取絕對值
+        target = abs(target);
+        int j = 1;
+        for (int i = 0; i < nums.size(); i++) {
+            // j 一定要在 i 的右邊，避免 i == j 的情形
+            j = max(j, i + 1);
+            
+            while (j < nums.size() && nums[j] - nums[i] < target) {
+                j++;
+            }
+            if (j == nums.size()) {
+                break;
+            }
+            if (nums[j] - nums[i] == target) {
+                return {nums[i], nums[j]};
+            }
+        }
+        return {-1, -1};
+    }
+};
