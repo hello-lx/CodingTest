749a98715a68c84906e54be6fd18c46684561b8d
hello-lx <476954524@qq.com>
2022-02-06 15:49:38 +0800

    update to 667

--- 1/841.cpp
+++ 1/841.cpp
@@ -16,3 +16,37 @@ using namespace std;
 #define INT_MAX 99999999999
 
 
+struct Compare{
+    bool operator() (string &a, string &b){
+        return a.size() >= b.size();
+    }
+};
+
+
+class Solution {
+public:
+    string stringReplace(vector<string> &a, vector<string> &b, string &s) {
+        unordered_map<string, string> A2B;
+        for(int i=0; i<a.size(); i++) A2B[a[i]] = b[i];
+        sort(a.begin(), a.end(), Compare());
+
+        int l=0, r=s.size()-1;
+        while(l <= r)
+        {
+            for(int i=0; i<a.size(); i++)
+            {
+                if(r-l+1 < a[i].size()) continue;
+                string str = s.substr(l, (int)a[i].size());
+                if(str == a[i])
+                {
+                    s.replace(l, (int)str.size(), A2B[str]);
+                    l = l + a[i].size() - 1;
+                    break;
+                }
+            }
+            l++;
+        }
+
+        return s;
+    }
+};
