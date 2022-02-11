c8da7e3b558f483d5156c397672f1a6d2150f5c3
hello-lx <476954524@qq.com>
2022-02-11 14:23:05 +0800

    test

--- 1/594.cpp
+++ 1/594.cpp
@@ -30,7 +30,7 @@ public:
      */
       int strStr2(const char* source, const char* target) {
          // write your code here
-         if(!source||!target)
+         if(!source || !target)
             return -1;
          if(!*target)
             return 0;
@@ -57,9 +57,15 @@ public:
 
          for(l=1;l<len-1;++l){
             auto prev=lps[l];
+            cout << l << ": " << prev << ", ";
+            
             if(prev>0 && target[l+1]==target[prev])
-               lps[l]=lps[prev-1];
+            {
+                lps[l]=lps[prev-1];
+                cout << lps[prev-1] << ", ";
+            }
          }
+         cout << endl;
 
          cout << "lps2: ";
          for(int i=0; i<lps.size(); i++)
@@ -86,7 +92,7 @@ public:
 
 int main()
 {
-   char *source = "abcccbbcccbdef", *target = "bcbcb";
+   char *source = "abcccbbcccbdef", *target = "bcbcbcb";
    Solution s;
    s.strStr2(source, target);
    return 0;
